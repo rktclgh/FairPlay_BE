aedc5fbb7f0b412fe2110168c9cae033edb7311b
Yennie <lye0713@gmail.com>
2025-07-30 15:34:41 +0900

    feat:  부스 관련 엔티티 수정 (#17)

--- src/main/java/com/fairing/fairplay/user/entity/BoothAdmin.java
+++ src/main/java/com/fairing/fairplay/user/entity/BoothAdmin.java
@@ -1,18 +1,32 @@
 package com.fairing.fairplay.user.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import com.fairing.fairplay.user.entity.Users;
 
 @Entity
 @Getter
 @NoArgsConstructor
+@Table(name = "booth_admin")
 public class BoothAdmin {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "booth_admin_id")
-    private Long id;
+    @Column(name = "user_id")
+    private Long userId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    @JoinColumn(name = "user_id")
+    private Users user;
 
     @Column(name = "manager_name", nullable = false, length = 20)
     private String managerName;
