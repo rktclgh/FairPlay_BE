a11ed151e62f5e14e451d37cc95c19565a97720c
kang1979 <kangbyeongch1979@gmail.com>
2025-08-05 17:22:16 +0900

    refactor: 날짜 범위 검증 로직 예외 처리 개선

--- src/main/java/com/fairing/fairplay/statistics/controller/HourlyStatisticsController.java
+++ src/main/java/com/fairing/fairplay/statistics/controller/HourlyStatisticsController.java
@@ -5,6 +5,8 @@ import com.fairing.fairplay.statistics.service.hourly.HourlyAnalysisService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 
@@ -22,7 +24,7 @@ public class HourlyStatisticsController {
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate end
     ) {
         if (start.isAfter(end)) {
-            throw new IllegalArgumentException("시작 날짜는 종료 날짜보다 늦을 수 없습니다.");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "시작 날짜는 종료 날짜보다 늦을 수 없습니다.");
         }
         return hourlyAnalysisService.analyzeHourlyBookings(eventId, start, end);
     }
