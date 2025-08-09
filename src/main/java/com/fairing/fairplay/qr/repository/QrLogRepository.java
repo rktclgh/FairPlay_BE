9f9124ddb572bef3e026f4d0e60e2c7011ea3490
Happy-Lotus <happylotus145@gmail.com>
2025-08-09 15:34:36 +0900

    feat: QR 상태 로그, 체크인/체크아웃 로그 저장 구현

--- src/main/java/com/fairing/fairplay/qr/repository/QrLogRepository.java
+++ src/main/java/com/fairing/fairplay/qr/repository/QrLogRepository.java
@@ -1,10 +1,13 @@
 package com.fairing.fairplay.qr.repository;
 
+import com.fairing.fairplay.qr.entity.QrActionCode;
 import com.fairing.fairplay.qr.entity.QrLog;
+import com.fairing.fairplay.qr.entity.QrTicket;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QrLogRepository extends JpaRepository<QrLog, Long> {
   Optional<QrLog> findByActionCode_Code(String qrCode);
 
+  boolean existsByQrTicketAndActionCode_Code(QrTicket qrTicket, String actionCodeCode);
 }
