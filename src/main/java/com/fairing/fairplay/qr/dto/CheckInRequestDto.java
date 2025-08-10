5dbfbf99015a16c846342a61d49bc906991d2951
Happy-Lotus <happylotus145@gmail.com>
2025-08-11 01:05:43 +0900

    fix: private 설정 누락

--- src/main/java/com/fairing/fairplay/qr/dto/CheckInRequestDto.java
+++ src/main/java/com/fairing/fairplay/qr/dto/CheckInRequestDto.java
@@ -17,7 +17,7 @@ public class CheckInRequestDto {
   private Attendee attendee;//회원
   private String codeValue;// qr 또는 manual 실제 값
   private String codeType;// QR or MANUAL
-  boolean requireUserMatch;// 회원 체크인일때 참석자=로그인정보 일치하는지 여부 조회
+  private boolean requireUserMatch;// 회원 체크인일때 참석자=로그인정보 일치하는지 여부 조회
   private CustomUserDetails userDetails;// 회원
   private String qrActionCode; // CHECKED_IN, MANUAL_CHECKED_IN
 }
