3d556351fcccb3b6b2e86e1b90334d3d620b5301
kang1979 <kangbyeongch1979@gmail.com>
2025-08-12 08:50:30 +0900

    feature: 행사 리스트 행사 검색 카테고리별 예약 분포 메소드 추가

--- src/main/java/com/fairing/fairplay/statistics/repository/dailystats/DailyStatsCustomRepository.java
+++ src/main/java/com/fairing/fairplay/statistics/repository/dailystats/DailyStatsCustomRepository.java
@@ -1,10 +1,31 @@
 package com.fairing.fairplay.statistics.repository.dailystats;
 
 
+
+import com.fairing.fairplay.statistics.dto.reservation.AdminReservationStatsByCategoryDto;
+import com.fairing.fairplay.statistics.dto.reservation.AdminReservationStatsListDto;
 import com.fairing.fairplay.statistics.entity.reservation.EventDailyStatistics;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import java.time.LocalDate;
 import java.util.List;
 
 public interface DailyStatsCustomRepository {
     List<EventDailyStatistics> calculate(LocalDate targetDate);
+    List<AdminReservationStatsByCategoryDto> aggregatedReservationByCategory(
+            LocalDate startDate,
+            LocalDate endDate
+
+    );
+
+    List<AdminReservationStatsListDto> searchEventReservationWithRank(LocalDate startDate, LocalDate endDate, String keyword, String mainCategory, String subCategory);
+
+    Page<AdminReservationStatsListDto> aggregatedPopularity(
+            LocalDate startDate,
+            LocalDate endDate,
+            String mainCategory,
+            String subCategory,
+            Pageable pageable  // 페이징 정보 추가
+    );
 }
