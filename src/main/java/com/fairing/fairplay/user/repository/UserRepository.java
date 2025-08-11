d8760e7a17702fb1406d4cff46c617e782edcfbb
aogog <33886018+aogog@users.noreply.github.com>
2025-08-11 16:02:54 +0900

    Update UserRepository.java

--- src/main/java/com/fairing/fairplay/user/repository/UserRepository.java
+++ src/main/java/com/fairing/fairplay/user/repository/UserRepository.java
@@ -5,18 +5,15 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-<<<<<<< HEAD
 import org.springframework.stereotype.Repository;
 
 import com.fairing.fairplay.user.entity.Users;
-=======
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
->>>>>>> b54f95211f4b458ad32afdb371f89f84fa31bc28
 
 @Repository
 public interface UserRepository extends JpaRepository<Users, Long> {
@@ -30,14 +27,12 @@ public interface UserRepository extends JpaRepository<Users, Long> {
 
     Optional<Users> findByUserId(Long userId);
 
-<<<<<<< HEAD
     @Query("SELECT u FROM Users u WHERE u.roleCode.id <= 2")
     List<Users> findAdmin();
-=======
+    
     // ADMIN 권한을 가진 사용자들을 조회하여 첫 번째 사용자 반환
     @Query("SELECT u FROM Users u WHERE u.roleCode.code = :roleCode ORDER BY u.userId ASC")
     List<Users> findByRoleCodeCodeOrderByUserIdAsc(@Param("roleCode") String roleCode);
->>>>>>> b54f95211f4b458ad32afdb371f89f84fa31bc28
 
     // ADMIN 권한을 가진 첫 번째 사용자 조회
     default Optional<Users> findFirstByRoleCodeCode(String roleCode) {
