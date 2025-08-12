45ba78c1a27906be57cb140f59b98c3da65efd37
kang1979 <kangbyeongch1979@gmail.com>
2025-08-12 15:11:45 +0900

    refactor: 요청 파라미터 검증 로직 추가 및 메소드 명 변경

--- src/main/java/com/fairing/fairplay/statistics/controller/ReservationRateAnalysisController.java
+++ src/main/java/com/fairing/fairplay/statistics/controller/ReservationRateAnalysisController.java
@@ -4,7 +4,9 @@ import com.fairing.fairplay.statistics.dto.reservation.ReservationRateAnalysisDt
 import com.fairing.fairplay.statistics.service.reservation.ReservationRateAnalysisService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 
@@ -16,11 +18,14 @@ public class ReservationRateAnalysisController {
     private final ReservationRateAnalysisService reservationRateAnalysisService;
 
     @GetMapping("/reservationRate/{eventId}")
-    public ReservationRateAnalysisDto getHourlyStatisticsByDate(
+    public ReservationRateAnalysisDto getReservationRateAnalysis(
             @PathVariable Long eventId,
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate start,
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate end
     ) {
+        if (start.isAfter(end)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "시작 날짜는 종료 날짜보다 늦을 수 없습니다.");
+            }
         return reservationRateAnalysisService.reservationRateAnalysisDashboard(eventId, start, end);
     }
 }
