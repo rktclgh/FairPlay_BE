431f743733caf593084e9c85a8ac59b69c219476
rktclgh <songchih@icloud.com>
2025-08-13 17:27:38 +0900

    빈 중복등록 문제 임시 해결

--- src/main/java/com/fairing/fairplay/banner/repository/ApplyStatusCodeRepository.java
+++ src/main/java/com/fairing/fairplay/banner/repository/ApplyStatusCodeRepository.java
@@ -1,12 +1,12 @@
-package com.fairing.fairplay.banner.repository;
-
-import com.fairing.fairplay.event.entity.ApplyStatusCode;
-import org.springframework.data.jpa.repository.*;
-import org.springframework.data.repository.query.Param;
-
-import java.util.Optional;
-
-public interface ApplyStatusCodeRepository extends JpaRepository<ApplyStatusCode, Integer> {
-    @Query("select a.id from ApplyStatusCode a where a.code = :code")
-    Optional<Long> findIdByCode(@Param("code") String code);
-}
+//package com.fairing.fairplay.banner.repository;
+//
+//import com.fairing.fairplay.event.entity.ApplyStatusCode;
+//import org.springframework.data.jpa.repository.*;
+//import org.springframework.data.repository.query.Param;
+//
+//import java.util.Optional;
+//
+//public interface ApplyStatusCodeRepository extends JpaRepository<ApplyStatusCode, Integer> {
+//    @Query("select a.id from ApplyStatusCode a where a.code = :code")
+//    Optional<Long> findIdByCode(@Param("code") String code);
+//}
