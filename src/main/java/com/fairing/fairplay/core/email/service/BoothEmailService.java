ecc5934e57d8e5cb8b54f3a924370b5a3a8be6b1
Yennie <lye0713@gmail.com>
2025-08-20 10:49:41 +0900

    fix: 부스 결제 오류

--- src/main/java/com/fairing/fairplay/core/email/service/BoothEmailService.java
+++ src/main/java/com/fairing/fairplay/core/email/service/BoothEmailService.java
@@ -45,8 +45,8 @@ public class BoothEmailService extends AbstractEmailService {
             Integer price = (Integer) params[7];
             Long applicationId = (Long) params[8];
             subject = "[FairPlay] 부스 등록 신청이 승인되었습니다! 계정 안내 및 결제 요청";
-            // 참고: applicationId가 마지막에 두 번 전달되고 있습니다. 템플릿 확인이 필요합니다.
-            htmlContent = buildHtmlContent("booth-approval.html", "logo", eventTitle, boothTitle, boothEmail, tempPassword, boothTypeName, boothSize, price, applicationId, applicationId);
+            // 결제하기 버튼을 제거했지만 취소 링크에서 applicationId가 여전히 필요함
+            htmlContent = buildHtmlContent("booth-approval.html", "logo", eventTitle, boothTitle, boothEmail, tempPassword, boothTypeName, boothSize, price, applicationId);
         } else if ("cancel".equals(type)) {
             String cancelReason = HtmlUtils.htmlEscape((String) params[3]);
             subject = "[FairPlay] 부스 취소 확인";
