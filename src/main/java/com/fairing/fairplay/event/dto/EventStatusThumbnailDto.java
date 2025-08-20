1f04046dbf89ab0d1c26f6a27371d078e8b1ecba
rktclgh <songchih@icloud.com>
2025-08-21 02:43:35 +0900

    시간 관련 설정들 원복 ㅠㅠ

--- src/main/java/com/fairing/fairplay/event/dto/EventStatusThumbnailDto.java
+++ src/main/java/com/fairing/fairplay/event/dto/EventStatusThumbnailDto.java
@@ -1,5 +1,6 @@
 package com.fairing.fairplay.event.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 @Getter
@@ -7,10 +8,16 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class EventStatusThumbnailDto {
     private Boolean hidden;
-        private String thumbnailUrl;
+    private String thumbnailUrl;
     private FileUploadDto thumbnailFile;
+    
+    // 프론트엔드에서 보내지만 무시할 필드들
+    private String email;
+    private String titleKr;
+    private String titleEng;
 
     @Getter
     @Setter
