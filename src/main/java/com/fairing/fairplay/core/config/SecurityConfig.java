e4f94bf7f964157430f29ee894329af1f9c6aba1
rktclgh <songchih@icloud.com>
2025-09-05 23:25:05 +0900

    히어로섹션 파일 경로 오류 수정

--- src/main/java/com/fairing/fairplay/core/config/SecurityConfig.java
+++ src/main/java/com/fairing/fairplay/core/config/SecurityConfig.java
@@ -81,6 +81,7 @@ public class SecurityConfig {
                                 "/api/chat/rooms/**", // 채팅방 목록 조회만 허용
                                 "/api/chat/presence/status/**", // 사용자 온라인 상태 조회 허용
                                 "/api/uploads/**",
+                                "/uploads/**", // 로컬 파일 시스템의 정적 파일 서빙
                                 "/api/payments/complete", // PG사에서 호출하는 결제 완료 웹훅
                                 "/api/events/apply", // 행사 등록 신청
                                 "/api/events/apply/check",
